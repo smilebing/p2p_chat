2f797d22028816cfe7798a6e1b0eda97261be7df
blandshot <iblandshot@gmail.com>
2016-03-10 11:45:42 +0800

    online user

---
+++ P2PChat/Form_online_user.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace P2PChat
+{
+    partial class Form_online_user
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.treeView_user = new System.Windows.Forms.TreeView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // treeView_user
+            // 
+            this.treeView_user.Location = new System.Drawing.Point(12, 12);
+            this.treeView_user.Name = "treeView_user";
+            this.treeView_user.Size = new System.Drawing.Size(221, 440);
+            this.treeView_user.TabIndex = 0;
+            this.treeView_user.NodeMouseDoubleClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.treeView_user_NodeMouseDoubleClick);
+            this.treeView_user.DoubleClick += new System.EventHandler(this.treeView_user_DoubleClick);
+            this.treeView_user.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.treeView_user_MouseDoubleClick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(27, 506);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(137, 505);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form_online_user
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(245, 570);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.treeView_user);
+            this.Name = "Form_online_user";
+            this.Text = "Form_online_user";
+            this.Load += new System.EventHandler(this.Form_online_user_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView_user;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
---
+++ P2PChat/Form_online_user.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P2PChat
+{
+    public partial class Form_online_user : Form
+    {
+      // 每3s 发送心跳包
+
+        public Form_online_user()
+        {
+            InitializeComponent();
+        }
+
+        TreeNode root = new TreeNode();
+
+
+        private void Form_online_user_Load(object sender, EventArgs e)
+        {
+            root.Text = "在线用户";
+            treeView_user.Nodes.Add(root);
+            //发送心跳包
+           
+
+          
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            TreeNode node = new TreeNode();
+            node.Text = "button ";
+            root.Nodes.Add(node);
+        }
+
+        private void treeView_user_DoubleClick(object sender, EventArgs e)
+        {
+            Console.WriteLine();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            root.Nodes.Clear();
+        }
+
+        private void treeView_user_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void treeView_user_NodeMouseDoubleClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+            Console.WriteLine(e.Node.Text);
+        }
+    }
+}
